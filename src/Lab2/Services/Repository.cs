32aa396939a296e7daa15549397e39f29ed0e148
Eruano <fdgolovlev@gmail.com>
2023-10-19 00:17:37 +0300

    feat: Init method for repository

--- src/Lab2/Services/Repository.cs
+++ src/Lab2/Services/Repository.cs
@@ -39,4 +39,77 @@ public class Repository
 
         throw new InvalidOperationException();
     }
+
+    public void Init()
+    {
+        var gpus = new List<BasePart>
+        {
+            new GraphicCard("RX 5700XT", 8, 4, 1650, 150, 280, 114),
+            new GraphicCard("RTX 3060Ti", 8, 4, 1410, 200, 247, 120),
+        };
+        _database.Add(Parts.GraphicCard.ToString(), gpus);
+
+        var cpus = new List<BasePart>
+        {
+            new Cpu("i7-12700", "LGA1700", false, 190, 21),
+            new Cpu("Ryzen 5 7600", "AM5", true, 220, 23),
+        };
+        _database.Add(Parts.CPU.ToString(), cpus);
+
+        var coolers = new List<BasePart>
+        {
+            new CPUCooler("Intel Box", "LGA1700", 200),
+            new CPUCooler("Bad AMD", "AM5", 210),
+        };
+        _database.Add(Parts.CPUCooler.ToString(), coolers);
+
+        var dram = new List<BasePart>
+        {
+            new Dram("Samsung", "Y", 32, 3200, 4, 3, 60),
+            new Dram("Kingston", "Y", 16, 3133, 4, 3, 40),
+        };
+        _database.Add(Parts.DRAM.ToString(), dram);
+
+        var hdd = new List<BasePart>
+        {
+            new Hdd("Smart", 2, 6000, 30),
+            new Hdd("Samsung", 5, 7200, 30),
+        };
+        _database.Add(Parts.HDD.ToString(), hdd);
+
+        var motherboards = new List<BasePart>
+        {
+            new Motherboard("Asus", "LGA1700", 4, 6, 4, 1030, 4, new Bios(2, 22), false, 3200),
+            new Motherboard("MSI", "AM5", 4, 6, 4, 1030, 4, new Bios(2, 25), false, 3200),
+        };
+        _database.Add(Parts.Motherboard.ToString(), motherboards);
+
+        var cases = new List<BasePart>
+        {
+            new PcCase("Zalman", 10, 300, 150),
+            new PcCase("HyperX", 10, 240, 150),
+        };
+        _database.Add(Parts.PCCase.ToString(), cases);
+
+        var psus = new List<BasePart>
+        {
+            new Psu("Cougar", 600),
+            new Psu("AirCool", 550),
+        };
+        _database.Add(Parts.PSU.ToString(), psus);
+
+        var ssds = new List<BasePart>
+        {
+            new Ssd("Samsung", 2, 7000, 50, DriveConnection.Pci),
+            new Ssd("Kingston", 1, 6500, 40, DriveConnection.Pci),
+        };
+        _database.Add(Parts.SSD.ToString(), ssds);
+
+        var adapters = new List<BasePart>
+        {
+            new WiFiAdapter("Type1", 4, 20),
+            new WiFiAdapter("Type2", 4, 15),
+        };
+        _database.Add(Parts.WiFiAdapter.ToString(), adapters);
+    }
 }
