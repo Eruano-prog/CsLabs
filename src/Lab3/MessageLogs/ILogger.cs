541e4c1713b643749067b28cffb2d536b7641737
Фёдор Головлёв <fdgolovlev@gmail.com>
2023-11-09 14:00:27 +0300

    feat: add logger

---
+++ src/Lab3/MessageLogs/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.MessageDir;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.MessageLogs;
+
+public interface ILogger
+{
+    public static abstract ILogger TakeInstance();
+    public void WriteLog(Message message, bool result);
+}
---
+++ src/Lab3/MessageLogs/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Itmo.ObjectOrientedProgramming.Lab3.MessageDir;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.MessageLogs;
+
+public class Logger : ILogger
+{
+    private static Logger? _instance;
+
+    private Logger()
+    { }
+
+    public static ILogger TakeInstance()
+    {
+        if (_instance is null)
+        {
+            _instance = new Logger();
+        }
+
+        return _instance;
+    }
+
+    public void WriteLog(Message message, bool result)
+    {
+        Console.WriteLine($"Message {message?.Text} is {(result ? string.Empty : "not")} printed");
+    }
+}
