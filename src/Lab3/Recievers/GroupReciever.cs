50b4731a7c84ad1776ff3165789c8fb19b06119c
Фёдор Головлёв <fdgolovlev@gmail.com>
2023-11-11 14:03:39 +0300

    feat: add GroupReciever

--- src/Lab3/Recievers/GroupReciever.cs
+++ src/Lab3/Recievers/GroupReciever.cs
@@ -1,5 +1,22 @@
-﻿namespace Itmo.ObjectOrientedProgramming.Lab3.Recievers;
+﻿using System.Collections.ObjectModel;
+using Itmo.ObjectOrientedProgramming.Lab3.MessageDir;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Recievers;
 
 public class GroupReciever : BaseReciever
 {
+    private Collection<IReciever> recievers;
+
+    public GroupReciever(Collection<IReciever> recievers)
+    {
+        this.recievers = recievers;
+    }
+
+    public override void Recieve(Message message)
+    {
+        foreach (IReciever reciever in recievers)
+        {
+            reciever.Recieve(message);
+        }
+    }
 }
