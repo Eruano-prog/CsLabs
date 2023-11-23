f2ebbb328cff80aa95886133e99b12a2f4167118
Eruano <fdgolovlev@gmail.com>
2023-11-24 02:08:47 +0300

    feat add tests and Program.cs for testing

--- tests/Lab4.Tests/Test.cs
+++ tests/Lab4.Tests/Test.cs
@@ -1,4 +1,8 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab4.Iterator;
+﻿using Itmo.ObjectOrientedProgramming.Lab4.FileSystem;
+using Itmo.ObjectOrientedProgramming.Lab4.Handlers;
+using Itmo.ObjectOrientedProgramming.Lab4.Iterator;
+using Itmo.ObjectOrientedProgramming.Lab4.Models;
+using NSubstitute;
 using Xunit;
 
 namespace Itmo.ObjectOrientedProgramming.Lab4.Tests;
@@ -8,9 +12,25 @@ public class Test
     [Fact]
     public void Test1()
     {
-        var com = new Command("First Second");
-        Assert.Equal("First", com.Current);
-        com.MoveNext();
-        Assert.Equal("Second", com.Current);
+        var command = new Command("C:\\Program Files'");
+        LocalFyleSystem fileSystem = Substitute.For<LocalFyleSystem>();
+
+        var context = new CommandContext(command, fileSystem);
+
+        IHandler chain = new ConnectHandler()
+            .AddNext(new DisconnectHandler())
+            .AddNext(new TreeHandler())
+            .AddNext(new FileHandler());
+
+        chain.Handle(context);
+
+        fileSystem.Received(1).Connect("C:\\Program Files");
+
+        command = new Command("tree goto Git");
+        context = new CommandContext(command, fileSystem);
+
+        chain.Handle(context);
+
+        fileSystem.Received(1).ChangeDirectory("Git");
     }
 }
