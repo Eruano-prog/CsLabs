a171ab3abc23b574ef6524726f32f33e9157bb23
Фёдор Головлёв <fdgolovlev@gmail.com>
2023-11-23 18:22:18 +0300

    feat: add FileSystem abstraction

---
+++ src/Lab4/FileSystem/IFileSystem.cs
@@ -0,0 +1,14 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab4.FileSystem;
+
+public interface IFileSystem
+{
+    public IFileSystem Connect(string path);
+    public IFileSystem Disconnect();
+    public void ChangeDirectory(string path);
+    public void ListFiles();
+    public void OpenFile(string filename);
+    public void MoveFile(string firstPath, string secondPath);
+    public void CopyFile(string firstPath, string secondPath);
+    public void DeleteFile(string path);
+    public void RenameFile(string path, string name);
+}
---
+++ src/Lab4/FileSystem/LocalFyleSystem.cs
@@ -0,0 +1,49 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab4.FileSystem;
+
+public class LocalFyleSystem : IFileSystem
+{
+    public IFileSystem Connect(string path)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IFileSystem Disconnect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ChangeDirectory(string path)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ListFiles()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OpenFile(string filename)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void MoveFile(string firstPath, string secondPath)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CopyFile(string firstPath, string secondPath)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteFile(string path)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RenameFile(string path, string name)
+    {
+        throw new System.NotImplementedException();
+    }
+}
