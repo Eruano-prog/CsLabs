d66811738f826d54792924a60b1c4ac1e1456e68
Eruano <fdgolovlev@gmail.com>
2023-12-21 02:07:04 +0300

    feat: First step in database logic

---
+++ src/Lab5/Lab5.Infrastructure.Database/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Lab5.Application.Users.Abstractions.Repositories;
+using Lab5.Application.Users.Models.Users;
+using Npgsql;
+
+namespace Lab5.Infrastructure;
+
+public class UserRepository : IUserRepository
+{
+    private NpgsqlConnection _database;
+    public UserRepository()
+    {
+        string connectionString = "Host=localhost:5432;" +
+                                   "Username=postgres;" +
+                                   "Password=fedor2004;" +
+                                   "Database=postgresDB";
+        _database = new NpgsqlConnection(connectionString);
+        _database.Open();
+    }
+
+    public User? FindUserByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
