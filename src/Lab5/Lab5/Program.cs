fee7166def14bf66a3e4b857f339167a5e183294
Eruano <fdgolovlev@gmail.com>
2023-12-22 23:29:30 +0300

    feat: Dipendency injection without containers

---
+++ src/Lab5/Lab5/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lab5.Application.Abstractions.Repositories;
+using Lab5.Application.Contracts.Users;
+using Lab5.Application.Users;
+using Lab5.Application.Users.Abstractions.Repositories;
+using Lab5.Infrastructure;
+using Lab5.Presentation.Console;
+using Lab5.Presentation.Console.Scenarios;
+
+IUserRepository userRepository = new UserRepository();
+IAccountRepository accountRepository = new AccountRepository();
+
+IScenario scenarioChain = new LoginScenario("login");
+scenarioChain.SetNext(new CheckBalanceScenario("balance")
+        .SetNext(new GetMoneyScenario("get")
+        .SetNext(new ChooseAccountScenario("choose"))));
+
+IUserService userService = new UserService(userRepository, accountRepository);
+var scenario = new ScenarioRunner(scenarioChain, userService);
+
+scenario.Run();
